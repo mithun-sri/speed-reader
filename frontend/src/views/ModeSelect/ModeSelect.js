a33e427d1e3ec93ceaf254f689cb4c61de46348e
David Sadler <djs21@ic.ac.uk>
2024-01-16 18:05:37 +0000

    Added choose game mode text

--- frontend/src/views/ModeSelect/ModeSelect.js
+++ frontend/src/views/ModeSelect/ModeSelect.js
@@ -1,12 +1,16 @@
 import './ModeSelect.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Box from '@mui/material/Box';
 
 const ModeSelectView = () => {
   return (
     <div className="ModeSelect">
       <Header />
       <header className="ModeSelect-header">
+        <Box sx={{ margin: '20px', fontSize: 40, color: '#fff', fontFamily: 'JetBrains Mono, monospace', fontWeight: 'bolder' }}>
+          choose game mode.
+        </Box>
         <Footer />
       </header>
     </div>
