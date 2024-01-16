01978d363b6c5649a07a748fb3503171ea8b3cab
David Sadler <djs21@ic.ac.uk>
2024-01-16 17:51:19 +0000

    Added base of mode select screen

---
+++ frontend/src/views/ModeSelect/ModeSelect.js
@@ -0,0 +1,16 @@
+import './ModeSelect.css';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
+const ModeSelectView = () => {
+  return (
+    <div className="ModeSelect">
+      <Header />
+      <header className="ModeSelect-header">
+        <Footer />
+      </header>
+    </div>
+  );
+}
+
+export default ModeSelectView;
