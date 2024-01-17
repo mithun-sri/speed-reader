1e122dfaa0056a77e34666f382042105164cf5fe
David Sadler <djs21@ic.ac.uk>
2024-01-17 19:28:18 +0000

    [djs21/#22] Removed countdown component from PreGame view

--- frontend/src/views/PreGame/PreGame.tsx
+++ frontend/src/views/PreGame/PreGame.tsx
@@ -12,7 +12,6 @@ const PreGameView = () => {
     <div className="PreGame">
       <Header />
       <header className="PreGame-header">
-        <CountdownComponent duration={3} />
         <SpeedSlider updateValue={setWPM}/>
         <Footer />
       </header>
