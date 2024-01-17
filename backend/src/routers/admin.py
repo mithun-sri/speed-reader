e46e647740f676b7e070db3ed7de6e3c053ae830
Taeho <taehokimbusines@gmail.com>
2024-01-17 19:13:41 +0000

    [tk3718/#17] Add token and refresh token endpoint

--- backend/src/routers/admin.py
+++ backend/src/routers/admin.py
@@ -2,12 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
-
-@router.post("/auth/login", tags=["auth"])
-async def login():
-    pass
-
-
 @router.post("/quiz")
 async def create_quiz():
     pass
