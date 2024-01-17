5604782c2484a7ab3cd348645b56fd95038337f8
Taeho <taehokimbusines@gmail.com>
2024-01-17 19:13:41 +0000

    [tk3718/#17] Add token and refresh token endpoint

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -10,22 +10,6 @@ router = APIRouter(prefix="/user", tags=["user"])
 user_db = {}
 
 
-@router.post("/auth/signup", status_code=status.HTTP_201_CREATED, tags=["auth"])
-async def user_signup(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-) -> RegistrationUserRepsonse:
-    response = await register_user(form_data, user_db)
-
-    return response
-
-
-@router.post("/auth/login", tags=["auth"])
-async def user_login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    response = await login_user(form_data, user_db)
-
-    return response
-
-
 @router.get("/summary")
 def get_user_summary():
     pass
--- backend/src/utils/security/auth.py
+++ backend/src/utils/security/auth.py
@@ -13,9 +13,12 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
     issued_at = datetime.now(timezone.utc)
     expire = issued_at + expires_delta
     
-    to_encode.update({"exp": expire, "iat": issued_at})
+    to_encode.update({"sub": to_encode.get("email"), "exp": expire, "iat": issued_at})
 
     return jwt.encode(to_encode, key=ACCESS_TOKEN_SECRET_KEY, algorithm=ALGORITHM)
     
 def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()    
+    to_encode.update({"sub": to_encode.get("email")})
+    
     return jwt.encode(data, key=REFRESH_TOKEN_SECRET_KEY, algorithm=ALGORITHM)
