bfad8c32df635120025de4b3664ff14bba93b7c1
David Sadler <djs21@ic.ac.uk>
2024-01-18 12:16:16 +0000

    [djs21/#21] Added Carousel component to mode select view

--- frontend/src/views/ModeSelect/ModeSelect.tsx
+++ frontend/src/views/ModeSelect/ModeSelect.tsx
@@ -1,16 +1,14 @@
 import './ModeSelect.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import Box from '@mui/material/Box';
+import Carousel from '../../components/Carousel/Carousel';
 
 const ModeSelectView = () => {
   return (
     <div className="ModeSelect">
       <Header />
       <header className="ModeSelect-header">
-        <Box sx={{ margin: '20px', fontSize: 40, color: '#fff', fontFamily: 'JetBrains Mono, monospace', fontWeight: 'bolder' }}>
-          choose game mode.
-        </Box>
+        <Carousel />
         <Footer />
       </header>
     </div>
