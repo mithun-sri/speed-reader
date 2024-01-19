400fad8ea3e0f9bdc757924648bef9ce90e73ec7
David Sadler <djs21@ic.ac.uk>
2024-01-19 15:31:25 +0000

    [djs21/#22] Added route for pre game view in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StandardModeGameView from "./views/StandardMode/StandardMode";
+import PreGameView from "./views/PreGame/PreGame";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<ModeSelectView />} /> */}
+          <Route path="/pre-game" element={<PreGameView />} />
           <Route path="/mode-1-game" element={<StandardModeGameView />} />
           <Route path="/mode-2-game" />
           <Route path="/mode-3-game" />
