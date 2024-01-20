01b14311674e824c72fddebff6e675a1b65ff4eb
Mithun Srinavaratnarajah <lnsmirth@googlemail.com>
2024-01-20 18:06:16 +0000

    [ms2921/#12] Update error handling for unknown texts

--- backend/src/database/database.py
+++ backend/src/database/database.py
@@ -3,14 +3,16 @@ from sqlalchemy.orm import Session
 from contextlib import closing
 import os
 
-DATABASE_URL = "postgresql://postgres:dfef07d5f299649990b0602eb709f942@cloud-vm-42-173.doc.ic.ac.uk:5432/speed_reader_database"
+DATABASE_URL = "postgresql://postgres:dfef07d5f299649990b0602eb709f942@dokku-postgres-speed-reader-database:5432/speed_reader_database"
+
 try:
     engine = create_engine(DATABASE_URL, pool_pre_ping=True)
-    with engine.connect() as connection:
+    with engine.connect():
         print("Database connection established")
 except Exception as e:
     print("Database connection failed")
     print(e)
+
 metadata = MetaData()
 
 text_table = Table(
@@ -30,4 +32,4 @@ def get_db():
         with closing(Session(bind=engine)) as db:
             yield db
     finally:
-        pass
+        pass  # Add cleanup or disconnect actions if needed
--- backend/src/routers/game.py
+++ backend/src/routers/game.py
@@ -1,30 +1,32 @@
-from ..database.database import text_table, get_db
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import func
 from sqlalchemy.orm import Session
+from ..database.database import text_table, get_db
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
 router = APIRouter(prefix="/game", tags=["game"])
-
 
 @router.get("/texts")
 async def get_texts(db: Session = Depends(get_db)):
-    logging.debug("Getting texts")
-    result = db.execute(text_table.select().order_by(func.random())).first()
-    logging.debug(result)
-    print(result)
-    if result is None:
-        return {"text": "No texts found"}
-    return {"text": result.content}
+    try:
+        result = db.execute(text_table.select().order_by(func.random())).first()
+
+        if result is None:
+            raise HTTPException(status_code=404, detail="No texts found")
+
+        return {"text": result.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
 @router.get("/texts/{id}")
 async def get_text(id: int, db: Session = Depends(get_db)):
-    logging.debug("Getting text id")
-    text = db.query(text_table).filter_by(text_id=id).first()
-    logging.debug(text)
-    print(text)
-    if text is None:
-        return {"text": "No texts found"}
-    return {"text": text.content}
+    try:
+        text = db.query(text_table).filter_by(text_id=id).first()
+
+        if text is None:
+            raise HTTPException(status_code=404, detail="Text not found")
+
+        return {"text": text.content}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal Server Error")
