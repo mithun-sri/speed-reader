b23286f020bf39d65708214584014eb2138d2f12
Taeho <taehokimbusines@gmail.com>
2024-01-22 11:36:43 +0000

    [tk3718/#19] Add GameProgressBar component

---
+++ frontend/src/components/ProgressBar/GameProgressBar.tsx
@@ -0,0 +1,25 @@
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
+import React, { useEffect, useState } from "react";
+
+const GameProgressBar: React.FC<{
+    gameProgress: number;
+}> = ({ gameProgress }) => {
+    const [progress, setProgress] = useState(0);
+
+    useEffect(() => {
+        setProgress(gameProgress);        
+    }, [gameProgress]);
+
+    return (
+        <Box 
+            sx={{ 
+                width: "70%",
+                alignContent: "center"
+            }}>
+            <LinearProgress variant="determinate" value={gameProgress} sx={{ backgroundColor: '#646669',  '.MuiLinearProgress-bar': { backgroundColor: '#B49A3A' } }}/>
+        </Box>
+    )
+}
+
+export default GameProgressBar;
