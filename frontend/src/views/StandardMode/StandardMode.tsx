f27df73b253d591a7f7e4936f73752e36b28950a
christin-s <christin.wulandari21@imperial.ac.uk>
2024-01-23 23:43:22 +0000

    [cyw321/#43] Implemented mode 1.2 to display multiline & justified text

--- frontend/src/views/StandardMode/StandardMode.tsx
+++ frontend/src/views/StandardMode/StandardMode.tsx
@@ -3,12 +3,17 @@ import Footer from "../../components/Footer/Footer";
 import CountdownComponent from "../../components/Counter/Counter";
 import { STANDARD_MODE_1 } from "../../common/constants";
 import JetBrainsMonoText from "../../components/Text/TextComponent";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./StandardMode.css";
-import axios, { AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosResponse, AxiosError } from "axios";
 
 import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
+
+export enum StandardMode {
+  Word = 0,
+  Justified = 1,
+}
 
 interface TextsApiResponse {
   text: string;
@@ -16,18 +21,22 @@ interface TextsApiResponse {
 
 const StandardModeGameView: React.FC<{
   wpm?: number;
-}> = ({ wpm }) => {
-  const [text, setText] = useState("The quick brown fox jumped over the lazy dog");
+  mode?: StandardMode;
+}> = ({ wpm, mode }) => {
+  const [text, setText] = useState(
+    "The quick brown fox jumped over the lazy dog",
+  );
 
   useEffect(() => {
     const config = {
-      method: 'get',
+      method: "get",
       maxBodyLength: Infinity,
       url: `${process.env.API_HOSTNAME}/api/v1/game/texts`,
-      headers: { }
+      headers: {},
     };
 
-    axios.request(config)
+    axios
+      .request(config)
       .then((response: AxiosResponse<TextsApiResponse>) => {
         console.log(JSON.stringify(response.data));
         setText(response.data.text);
@@ -35,8 +44,7 @@ const StandardModeGameView: React.FC<{
       .catch((error: AxiosError) => {
         console.log(error);
       });
-  }
-  , []);
+  }, []);
 
   const [showGameScreen, setShowGameScreen] = useState(false);
 
@@ -56,7 +64,11 @@ const StandardModeGameView: React.FC<{
     <Box>
       <Header />
       {showGameScreen ? (
-        <StandardModeGameComponent wpm={wpm || 200} text={text} />
+        <StandardModeGameComponent
+          wpm={wpm || 200}
+          text={text}
+          mode={mode || StandardMode.Word}
+        />
       ) : (
         countdownComp
       )}
@@ -67,12 +79,14 @@ const StandardModeGameView: React.FC<{
 
 StandardModeGameView.propTypes = {
   wpm: PropTypes.number,
+  mode: PropTypes.oneOf([StandardMode.Word, StandardMode.Justified]),
 };
 
 const StandardModeGameComponent: React.FC<{
   wpm: number;
   text: string;
-}> = ({ wpm, text }) => {
+  mode: StandardMode;
+}> = ({ wpm, text, mode }) => {
   return (
     <Box
       sx={{
@@ -82,28 +96,11 @@ const StandardModeGameComponent: React.FC<{
         alignItems: "center",
       }}
     >
-      <ReadingTextDisplay text={text} wpm={wpm} />
-      <Box
-        sx={{
-          padding: "10px",
-          width: "50%",
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "start",
-        }}
-      >
-        <JetBrainsMonoText
-          text="WPM: "
-          size={15}
-          color="#fff"
-        ></JetBrainsMonoText>
-        <Box style={{ marginTop: "10px" }}></Box>
-        <JetBrainsMonoText
-          text="Words remaining: "
-          size={15}
-          color="#fff"
-        ></JetBrainsMonoText>
-      </Box>
+      {mode == StandardMode.Word ? (
+        <WordTextDisplay text={text} wpm={wpm} />
+      ) : (
+        <JustifiedTextDisplay text={text} wpm={wpm} />
+      )}
     </Box>
   );
 };
@@ -111,6 +108,7 @@ const StandardModeGameComponent: React.FC<{
 StandardModeGameComponent.propTypes = {
   wpm: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
+  mode: PropTypes.oneOf([StandardMode.Word, StandardMode.Justified]).isRequired,
 };
 
 const nonHighlightedWord: React.FC<{
@@ -137,7 +135,11 @@ const highlightedWord: React.FC<{
   );
 };
 
-const ReadingTextDisplay: React.FC<{
+/*
+  Mode 1.1 (Word): Standard mode displaying the text one word at a time 
+  based on the specified words per minute (wpm).
+*/
+const WordTextDisplay: React.FC<{
   text: string;
   wpm: number;
   size?: number;
@@ -154,7 +156,7 @@ const ReadingTextDisplay: React.FC<{
     }
     if (event.code === "ArrowDown") {
       console.log("ArrowDown");
-      setWpm(curr_wpm - 5);
+      setWpm(Math.max(curr_wpm - 5, 1));
       console.log(curr_wpm);
     }
   };
@@ -195,7 +197,90 @@ const ReadingTextDisplay: React.FC<{
   );
 };
 
-ReadingTextDisplay.propTypes = {
+WordTextDisplay.propTypes = {
+  text: PropTypes.string.isRequired,
+  wpm: PropTypes.number.isRequired,
+  size: PropTypes.number,
+};
+
+/*
+  Mode 1.2 (Justified): Standard mode displaying the entire text in a justified format across multiple lines.
+  Words are highlighted sequentially based on the specified words per minute (wpm).
+*/
+const JustifiedTextDisplay: React.FC<{
+  text: string;
+  wpm: number;
+  size?: number;
+}> = ({ text, wpm, size }) => {
+  const [highlightedIndex, setHighlightedIndex] = useState(0);
+  const [curr_wpm, setWpm] = useState(wpm);
+
+  const handleKeyDown = (event: KeyboardEvent): void => {
+    if (event.code === "ArrowUp") {
+      console.log("ArrowUp");
+      setWpm(curr_wpm + 5);
+      console.log(curr_wpm);
+    }
+    if (event.code === "ArrowDown") {
+      console.log("ArrowDown");
+      setWpm(Math.max(curr_wpm - 5, 1));
+      console.log(curr_wpm);
+    }
+  };
+  // updates WPM based on keyboard event
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [curr_wpm]);
+
+  // updates which word to be highlighted
+  useEffect(() => {
+    const wordsArray = text.split(" ");
+
+    const interval = setInterval(() => {
+      setHighlightedIndex((prevIndex) => {
+        const newIndex = prevIndex + 1;
+        return newIndex < wordsArray.length ? newIndex : prevIndex;
+      });
+    }, 60000 / curr_wpm);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [text, curr_wpm]);
+
+  return (
+    <Box
+      sx={{
+        width: "500px",
+        padding: "25px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      {text.split(" ").map((word, index) => (
+        <Box
+          component="span"
+          key={index}
+          sx={{
+            marginRight: "8px",
+          }}
+        >
+          <JetBrainsMonoText
+            text={word}
+            size={25}
+            color={index <= highlightedIndex ? "#E2B714" : "#646669"}
+          ></JetBrainsMonoText>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+
+JustifiedTextDisplay.propTypes = {
   text: PropTypes.string.isRequired,
   wpm: PropTypes.number.isRequired,
   size: PropTypes.number,
