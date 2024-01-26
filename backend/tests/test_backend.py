6738d86ca2644f9240afb6d1f3e0a683cc7d6e24
djs21 <djs21@ic.ac.uk>
2024-01-26 12:43:53 +0000

    [djs21/#10] Added unit test for texts endpoint

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -1,5 +1,14 @@
 from src import __version__
+from fastapi.testclient import TestClient
+from src.main import app
 
+test_client = TestClient(app)
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_texts():
+    response = test_client.get("/texts")
+    assert response.status_code == 200
+    assert "text" in response.json().keys()
+    assert isinstance(response.json()["text"], str)
