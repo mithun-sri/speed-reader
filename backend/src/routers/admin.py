09ba9f1a905aa8521c5c3f6b1897a731c4444791
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-01 21:55:22 +0000

    [tm821/#106] Add TODO comments to files where conflicts are likely

--- backend/src/routers/admin.py
+++ backend/src/routers/admin.py
@@ -5,11 +5,14 @@ from ..logger import LoggerRoute
 router = APIRouter(prefix="/admin", tags=["admin"], route_class=LoggerRoute)
 
 
+# TODO: Change `/quiz` to `/questions`.
+# TODO: Do not use "question" and "quiz" interchangeably i.e. follow the database table name.
 @router.post("/quiz")
 async def create_quiz():
     pass
 
 
+# TODO: Rename to `get_dashboard_data`.
 @router.get("/dashboard")
 async def get_admin_dashboard():
     pass
--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -1,14 +1,11 @@
-from typing import Any
-
 from fastapi import APIRouter
 
 from ..logger import LoggerRoute
 
 router = APIRouter(prefix="/user", tags=["user"], route_class=LoggerRoute)
 
-user_db: dict[str, Any] = {}
 
-
+# TODO: Create dedicated endpoints for stats, available texts and recent games.
 @router.get("/summary")
 def get_user_summary():
     pass
