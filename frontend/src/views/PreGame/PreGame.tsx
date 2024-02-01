62e039339a21607cdbc15b3912eb42eff5ccdc55
Taeho <taehokimbusines@gmail.com>
2024-02-01 23:20:42 +0000

    [tk3718/#73] Fixed unused import

--- frontend/src/views/PreGame/PreGame.tsx
+++ frontend/src/views/PreGame/PreGame.tsx
@@ -1,5 +1,4 @@
 import StartButton from "../../components/Button/StartButton";
-import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import SpeedSlider from "../../components/Slider/Slider";
 import "./PreGame.css";
