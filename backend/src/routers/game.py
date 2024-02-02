10c15d64cec77a6c53f851a846ea4f8dfa7ddeb8
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-02 12:08:42 +0000

    [tm821/#108] Fix get_questions endpoint to return 3 random questions

--- backend/src/routers/game.py
+++ backend/src/routers/game.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
@@ -39,23 +41,31 @@ async def get_text(
     return text
 
 
-# Collects three questions at random from the database
 @router.get(
-    "/texts/{text_id}/questions",
+    "/texts/{text_id}/questions/random",
     response_model=list[schemas.Question],
 )
-async def get_questions(
+async def get_random_questions(
     text_id: str,
     session: Session = Depends(get_session),
 ):
+    """
+    Get 3 random questions associated with a text
+    """
     text = session.get(models.Text, text_id)
     if text is None:
         raise HTTPException(status_code=404, detail="Text not found")
 
-    return text.questions
+    num_questions = 3
+    if len(text.questions) < num_questions:
+        raise HTTPException(
+            status_code=404,
+            detail="Text does not have enough questions",
+        )
+
+    return random.sample(text.questions, num_questions)
 
 
-# Calculates quiz results
 @router.post(
     "/texts/{text_id}/answers",
     response_model=list[schemas.QuestionResult],
@@ -64,6 +74,9 @@ async def submit_answers(
     answers: list[schemas.QuestionAnswer],
     session: Session = Depends(get_session),
 ):
+    """
+    Accepts a list of answers and returns the results
+    """
     results = []
     for answer in answers:
         question = session.get(models.Question, answer.question_id)
