8a03480ba35bf5e00305f81feaa2968885eeb1ed
Mithun Srinavaratnarajah <lnsmirth@googlemail.com>
2024-02-03 21:24:19 +0000

    [ms2921/#85] Add custom sorting and filtering criteria for endpoint

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends, Query
-from sqlalchemy import func, select
+from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from ..database import get_session
@@ -16,32 +18,53 @@ def get_user_summary():
     pass
 
 
+class TextFilter(BaseModel):
+    page: int = Query(1, ge=1)
+    page_size: int = Query(10, ge=1, le=100)
+    difficulty: Optional[str] = Query(None)
+    game_mode: Optional[int] = Query(None)
+    sort: Optional[str] = Query(None)
+
+
 @router.get("/available_texts")
 async def get_available_texts(
     user_id: str,
-    page: int = Query(1, ge=1),
-    page_size: int = Query(10, ge=1, le=100),
+    text_filter: TextFilter = Depends(),
     session: Session = Depends(get_session),
 ):
+    page, page_size, difficulty, game_mode, sort = (
+        text_filter.page,
+        text_filter.page_size,
+        text_filter.difficulty,
+        text_filter.game_mode,
+        text_filter.sort,
+    )
     # Collect text_ids of texts read by user in user.history from MongoDB
     texts_read_by_user = History.objects(  # pylint: disable=no-member
         user_id=user_id
     ).distinct("text_id")
 
+    # Build the base query for texts not read by user
+    base_query = session.query(Text).filter(~Text.id.in_(texts_read_by_user))
+
+    if difficulty:
+        base_query = base_query.filter(Text.difficulty == difficulty)
+    if game_mode is not None:
+        base_query = base_query.filter(Text.game_mode == game_mode)
+    # Sort the texts by the given attribute
+    if sort:
+        if sort.startswith("-"):
+            attr = getattr(Text, sort[1:])
+            base_query = base_query.order_by(attr.desc())
+        else:
+            attr = getattr(Text, sort)
+            base_query = base_query.order_by(sort)
+
     # Calculate the total number of texts not read by the user
-    total_texts = (
-        session.query(func.count(Text.id))
-        .filter(~Text.id.in_(texts_read_by_user))
-        .scalar()
-    )
+    total_texts = base_query.count()
 
     # Collect paginated texts from PostgreSQL of texts not read by user
-    query = (
-        select(Text)
-        .where(~Text.id.in_(texts_read_by_user))
-        .offset((page - 1) * page_size)
-        .limit(page_size)
-    )
+    query = base_query.offset((page - 1) * page_size).limit(page_size)
     results = session.scalars(query)
 
     response = {
