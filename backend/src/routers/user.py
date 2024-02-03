2082d0565ebb44b2daf7116f4d463ab511585496
Mithun Srinavaratnarajah <lnsmirth@googlemail.com>
2024-02-03 16:04:41 +0000

    [ms2921/#85] Create skeleton for available_text endpoint

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from ..logger import LoggerRoute
 
@@ -8,4 +8,13 @@ router = APIRouter(prefix="/user", tags=["user"], route_class=LoggerRoute)
 # TODO: Create dedicated endpoints for stats, available texts and recent games.
 @router.get("/summary")
 def get_user_summary():
+    pass
+
+# TODO: Create endpoint for collecting available texts for users.
+@router.get("{user_id}/available_texts")
+def get_available_texts(
+    user_id: str,
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100),
+):
     pass
