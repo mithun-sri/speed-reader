e762bb4de1587e5a9e767cc39249faf565b8bad7
Taeho <taehokimbusines@gmail.com>
2024-02-04 00:09:15 +0000

    [tk3718/#19] Format code using Prettier

--- frontend/src/components/Results/Score.tsx
+++ frontend/src/components/Results/Score.tsx
@@ -27,27 +27,34 @@ const Score: React.FC<{
 
   return (
     <Box>
-      <Box sx={{ 
-        fontSize: circleWidth / 4.2, 
-        fontWeight: "bolder", 
-        fontFamily: "JetBrains Mono, monospace",
-        margin: "30px",
-        paddingRight: circleWidth / 14
-      }}>
+      <Box
+        sx={{
+          fontSize: circleWidth / 4.2,
+          fontWeight: "bolder",
+          fontFamily: "JetBrains Mono, monospace",
+          margin: "30px",
+          paddingRight: circleWidth / 14,
+        }}
+      >
         score.
       </Box>
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <CircularProgress sx = {{color: "#E2B714" }} size={circleWidth} variant="determinate" value={score} />
+      <Box sx={{ position: "relative", display: "inline-flex" }}>
+        <CircularProgress
+          sx={{ color: "#E2B714" }}
+          size={circleWidth}
+          variant="determinate"
+          value={score}
+        />
         <Box
           sx={{
             top: 0,
             left: 0,
             bottom: 0,
             right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
           <Typography
