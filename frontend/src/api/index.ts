a8933efa17c391e70c6672455d968b137327b170
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-05 16:31:55 +0000

    [tm821/#114] Update auto-generated API client

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -9,7 +9,10 @@ export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { Body_authenticate_user_auth_token_post } from './models/Body_authenticate_user_auth_token_post';
 export type { HTTPValidationError } from './models/HTTPValidationError';
-export type { QuizAnswers } from './models/QuizAnswers';
+export type { Question } from './models/Question';
+export type { QuestionAnswer } from './models/QuestionAnswer';
+export type { QuestionResult } from './models/QuestionResult';
+export type { Text } from './models/Text';
 export type { ValidationError } from './models/ValidationError';
 
 export { AdminService } from './services/AdminService';
