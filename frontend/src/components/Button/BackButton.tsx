72e909c0f0ae0dc3366ee629f2fdc3c6aef63154
christin-s <christin.wulandari21@imperial.ac.uk>
2024-02-06 22:45:51 +0000

    [cyw321/#70] Implement back button component

---
+++ frontend/src/components/Button/BackButton.tsx
@@ -0,0 +1,51 @@
+import { IconButton } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useGameScreenContext } from "../../views/GameScreen/GameScreen";
+
+const BackButton: React.FC<{
+  label?: string;
+}> = ({ label }) => {
+  const { incrementCurrentStage } = useGameScreenContext();
+  const [fontSize, setFontSize] = useState(calculateFontSize());
+
+  useEffect(() => {
+    function handleResize() {
+      setFontSize(calculateFontSize());
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function calculateFontSize() {
+    const windowWidth = window.innerWidth;
+    const minSize = 40;
+    const maxSize = 180;
+    const calculatedSize = Math.min(
+      maxSize,
+      Math.max(minSize, windowWidth / 6),
+    );
+    return calculatedSize;
+  }
+
+  const buttonText: string = label || "back";
+
+  return (
+    <IconButton
+      onClick={() => {
+        incrementCurrentStage();
+      }}
+      sx={{
+        fontFamily: "JetBrains Mono, monospace",
+        color: "#FFFFFF",
+        fontWeight: "bolder",
+        fontSize: fontSize / 6.3,
+      }}
+    >
+      {`<_${buttonText}`}
+    </IconButton>
+  );
+};
+
+export default BackButton;
