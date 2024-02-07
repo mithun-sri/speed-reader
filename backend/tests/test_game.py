480f876874d000afe374f60ac8da4f221e860bad
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-07 18:41:36 +0000

    [tm821/#88] Rewrite unit tests for game endpoints after refactoring changes

--- backend/tests/test_game.py
+++ backend/tests/test_game.py
@@ -1,19 +1,27 @@
+import pytest
 import ulid
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 from src import schemas
+from src.factories.history import HistoryFactory
 from src.factories.question import QuestionFactory
 from src.factories.text import TextFactory
+from src.factories.user import UserFactory
 
 
 class TestGetNextText:
-    @staticmethod
-    def test_returns_404_when_text_table_is_empty(client: TestClient):
+    def test_returns_404_when_text_table_is_empty(
+        self,
+        client: TestClient,
+    ):
         response = client.get("/api/v1/game/texts/next")
         assert response.status_code == 404
 
-    @staticmethod
-    def test_returns_text(client: TestClient, session: Session):
+    def test_returns_text(
+        self,
+        client: TestClient,
+        session: Session,
+    ):
         text = TextFactory.build()
         session.add(text)
         session.commit()
@@ -26,19 +34,24 @@ class TestGetNextText:
 
 
 class TextGetText:
-    @staticmethod
-    def test_returns_404_when_no_text_matches_id(client: TestClient, session: Session):
+    def test_returns_404_when_no_text_matches_id(
+        self,
+        client: TestClient,
+        session: Session,
+    ):
         text = TextFactory.build()
         session.add(text)
         session.commit()
 
         new_text_id = str(ulid.new())
-        assert text.id != new_text_id
         response = client.get(f"/api/v1/game/texts/{new_text_id}")
         assert response.status_code == 404
 
-    @staticmethod
-    def test_returns_requested_text(client: TestClient, session: Session):
+    def test_returns_requested_text(
+        self,
+        client: TestClient,
+        session: Session,
+    ):
         texts = TextFactory.build_batch(5)
         session.add_all(texts)
         session.commit()
@@ -49,8 +62,11 @@ class TextGetText:
 
 
 class TestGetQuestions:
-    @staticmethod
-    def test_returns_all_questions_for_the_text(client: TestClient, session: Session):
+    def test_returns_all_questions_for_the_text(
+        self,
+        client: TestClient,
+        session: Session,
+    ):
         text = TextFactory.build()
         questions = QuestionFactory.build_batch(10, text=text)
         session.add(text)
@@ -61,77 +77,106 @@ class TestGetQuestions:
         assert response.status_code == 200
 
         response_body = response.json()
-        assert len(response_body) == 3  # picks 3 questions
+        assert len(response_body) == 10  # Picks 10 questions
         # fmt: off
         assert set(question["id"] for question in response_body) \
             .issubset(question.id for question in questions)
 
 
 class TestSubmitAnswers:
-    @staticmethod
-    def test_returns_404_if_question_does_not_exist(
-        client: TestClient, session: Session
+    @pytest.fixture(autouse=True)
+    def setup_fixture(
+        self,
+        session: Session,
     ):
-        text = TextFactory.build()
-        session.add(text)
+        self.user = UserFactory.build()
+        self.text = TextFactory.build()
+        self.questions = QuestionFactory.build_batch(10, text=self.text)
+        session.add(self.text)
         session.commit()
 
-        question_id = str(ulid.new())
-        response = client.post(
-            f"/api/v1/game/texts/{text.id}/answers",
-            json=[
-                schemas.QuestionAnswer(
-                    question_id=question_id,
-                    selected_option=0,
-                ).model_dump()
-            ],
+        self.history = HistoryFactory.build(
+            text_id=self.text.id,
+            user_id=self.user.id,
+            question_ids=[question.id for question in self.questions],
+            game_mode=self.text.game_mode,
         )
+
+    def test_returns_404_if_question_does_not_exist(
+        self,
+        client: TestClient,
+    ):
+        response = client.post(
+            f"/api/v1/game/texts/{self.text.id}/answers",
+            json={
+                "answers": [
+                    schemas.QuestionAnswer(
+                        question_id=str(ulid.new()),
+                        selected_option=question.correct_option,
+                    ).model_dump()
+                    for question in self.questions
+                ],
+                "average_wpm": self.history.average_wpm,
+                "interval_wpms": self.history.interval_wpms,
+                "game_mode": self.history.game_mode,
+                "game_submode": self.history.game_submode,
+            },
+        )
+        print(response.content)
         assert response.status_code == 404
 
-    @staticmethod
     def test_returns_400_if_questions_do_not_match_text_id(
-        client: TestClient, session: Session
+        self,
+        client: TestClient,
+        session: Session,
     ):
-        text1 = TextFactory.build()
-        text2 = TextFactory.build()
-        question = QuestionFactory.build(text=text2)
-        session.add(text1)
-        session.add(text2)
-        session.add(question)
+        new_text = TextFactory.build()
+        new_questions = QuestionFactory.build_batch(10, text=new_text)
+        session.add(new_text)
+        session.add_all(new_questions)
         session.commit()
 
         response = client.post(
-            f"/api/v1/game/texts/{text1.id}/answers",
-            json=[
-                schemas.QuestionAnswer(
-                    question_id=question.id,
-                    selected_option=0,
-                ).model_dump()
-            ],
+            f"/api/v1/game/texts/{self.text.id}/answers",
+            json={
+                "answers": [
+                    schemas.QuestionAnswer(
+                        question_id=question.id,
+                        selected_option=question.correct_option,
+                    ).model_dump()
+                    for question in new_questions
+                ],
+                "average_wpm": self.history.average_wpm,
+                "interval_wpms": self.history.interval_wpms,
+                "game_mode": self.history.game_mode,
+                "game_submode": self.history.game_submode,
+            },
         )
         assert response.status_code == 400
 
-    @staticmethod
-    def test_returns_results_and_correct_answers(client: TestClient, session: Session):
-        text = TextFactory.build()
-        questions = QuestionFactory.build_batch(5, text=text)
-        session.add(text)
-        session.add_all(questions)
-        session.commit()
-
+    def test_returns_results_and_correct_answers(
+        self,
+        client: TestClient,
+    ):
         response = client.post(
-            f"/api/v1/game/texts/{text.id}/answers",
-            json=[
-                schemas.QuestionAnswer(
-                    question_id=question.id,
-                    selected_option=question.correct_option,
-                ).model_dump()
-                for question in questions
-            ],
+            f"/api/v1/game/texts/{self.text.id}/answers",
+            json={
+                "answers": [
+                    schemas.QuestionAnswer(
+                        question_id=question.id,
+                        selected_option=question.correct_option,
+                    ).model_dump()
+                    for question in self.questions
+                ],
+                "average_wpm": self.history.average_wpm,
+                "interval_wpms": self.history.interval_wpms,
+                "game_mode": self.history.game_mode,
+                "game_submode": self.history.game_submode,
+            },
         )
         assert response.status_code == 200
         data = response.json()
-        assert len(data) == len(questions)
+        assert len(data) == len(self.questions)
         assert all(
             result["correct"] and result["selected_option"] == result["correct_option"]
             for result in data
