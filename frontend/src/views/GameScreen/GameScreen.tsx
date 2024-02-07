05e195187b5a08a3f4f564758352617c74b5a665
christin-s <christin.wulandari21@imperial.ac.uk>
2024-02-07 01:33:49 +0000

    [cyw321/#70] Move DiffSelect to a different file

--- frontend/src/views/GameScreen/GameScreen.tsx
+++ frontend/src/views/GameScreen/GameScreen.tsx
@@ -1,24 +1,16 @@
-import Box from "@mui/material/Box";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   ADAPTIVE_MODE,
-  EASY,
-  GameDifficulty,
-  HARD,
-  MED,
   STANDARD_MODE,
   SUMMARISED_ADAPTIVE_MODE,
 } from "../../common/constants";
-import BackButton from "../../components/Button/BackButton";
-import Carousel from "../../components/Carousel/Carousel";
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
 import { GameProvider, useGameContext } from "../../context/GameContext";
+import DiffSelect from "../DiffSelect/DiffSelect";
 import ModeSelectView from "../ModeSelect/ModeSelect";
-import WpmView from "../WpmView/WpmView";
 import Quiz from "../Quiz/Quiz";
 import StandardModeGameView from "../StandardMode/StandardMode";
 import StandardSubModeView from "../StandardMode/StandardSubMode";
+import WpmView from "../WpmView/WpmView";
 
 const GameScreenContext = React.createContext<{
   currentStage: number;
@@ -47,63 +39,6 @@ const StandardSelect = () => {
 
 const WpmSelect = () => {
   return <WpmView />;
-};
-
-const DiffSelect = () => {
-  const { incrementCurrentStage, decrementCurrentStage } =
-    useGameScreenContext();
-  const { difficulty, setDifficulty } = useGameContext();
-  const [valueFromCarousel, setValueFromCarousel] = useState<number>(-1); // To retrieve selected index from Carousel
-
-  const options: GameDifficulty[] = [EASY, MED, HARD];
-
-  const handleBackButton = () => {
-    setDifficulty(null);
-    decrementCurrentStage();
-  };
-
-  // Callback function to handle the selected index form the Carousel
-  const handleValueFromCarousel = (value: number) => {
-    setValueFromCarousel(value);
-  };
-
-  // setMode according to the selected index from options
-  useEffect(() => {
-    if (valueFromCarousel >= 0 && valueFromCarousel < options.length) {
-      setDifficulty(options[valueFromCarousel]);
-      incrementCurrentStage(); // Go to the next view in GameScreen
-    }
-  }, [valueFromCarousel, setDifficulty]);
-
-  return (
-    <Box>
-      <Header />
-      <Box sx={{ marginLeft: "7vw", marginTop: "35px" }}>
-        <BackButton label="mode" handleClick={handleBackButton} />
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: "#2c2e31",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          textAlign: "center",
-          color: "white",
-        }}
-      >
-        <Carousel
-          title="choose text difficulty."
-          options={options}
-          returnSelectedIndex={handleValueFromCarousel}
-          defaultIdx={
-            difficulty !== null ? options.indexOf(difficulty) : undefined
-          }
-        />
-        <Footer />
-      </Box>
-    </Box>
-  );
 };
 
 const GameView = () => {
