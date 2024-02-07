9d90fcda441359372acb5babd26643f79e9f7a71
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-07 00:55:17 +0000

    [tm821/#113] Create user factory

--- backend/src/factories/text.py
+++ backend/src/factories/text.py
@@ -11,8 +11,4 @@ class TextFactory(factory.alchemy.SQLAlchemyModelFactory):
     content = factory.Faker("text")
     difficulty = factory.Faker("random_element", elements=["easy", "medium", "hard"])
     game_mode = factory.Faker("random_int", min=1, max=2)
-
-    @factory.lazy_attribute
-    def word_count(self):
-        # pylint: disable=no-member
-        return len(self.content.split())
+    word_count = factory.LazyAttribute(lambda obj: len(obj.content.split()))
---
+++ backend/src/factories/user.py
@@ -0,0 +1,17 @@
+import factory
+
+from .. import models
+from ..utils.crypt import get_password_hash
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = models.User
+
+    class Params:
+        raw_password = factory.Faker("password")
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("safe_email")
+    password = factory.LazyAttribute(lambda obj: get_password_hash(obj.raw_password))
+    status = factory.Faker("random_element", elements=["active", "inactive"])
