79200905e2171d2ddb03e47d5094cc7b3e31b146
christin-s <christin.wulandari21@imperial.ac.uk>
2024-02-18 18:14:46 +0000

    [cyw321/#141] Create GPT Text, and Question Feed components

---
+++ frontend/src/components/Gpt/GptQuestionFeed.tsx
@@ -0,0 +1,30 @@
+import { Box } from "@mui/material";
+import { UseFormReturn } from "react-hook-form";
+import GptQuestion from "./GptQuestion";
+import { GptFormData } from "./GptSuggestions";
+
+const GptQuestionFeed: React.FC<{
+  useFormReturn: UseFormReturn<GptFormData>;
+}> = ({ useFormReturn }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormReturn;
+
+  const responseContainerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    gap: 4,
+  };
+
+  return (
+    <Box sx={responseContainerStyles}>
+      <GptQuestion />
+    </Box>
+  );
+};
+
+export default GptQuestionFeed;
---
+++ frontend/src/components/Gpt/GptText.tsx
@@ -0,0 +1,57 @@
+import { Box } from "@mui/material";
+import { UseFormReturn } from "react-hook-form";
+import JetBrainsMonoText from "../Text/TextComponent";
+import StyledTextField from "../Textbox/StyledTextField";
+import { GptFormData } from "./GptSuggestions";
+
+const GptText: React.FC<{
+  useFormReturn: UseFormReturn<GptFormData>;
+}> = ({ useFormReturn }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormReturn;
+
+  const containerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    gap: 6,
+    marginY: "50px",
+  };
+
+  const textFieldContainerStyles = {
+    display: "flex",
+    flexDirection: "column",
+    gap: 1,
+  };
+
+  const textLabelStyles = {
+    paddingLeft: "10px",
+  };
+
+  return (
+    <Box sx={containerStyles}>
+      <Box sx={textFieldContainerStyles}>
+        <Box sx={textLabelStyles}>
+          <JetBrainsMonoText text={"Text Title"} size={25} color={"white"} />
+        </Box>
+        <StyledTextField />
+      </Box>
+      <Box sx={textFieldContainerStyles}>
+        <Box sx={textLabelStyles}>
+          <JetBrainsMonoText text={"Text Content"} size={25} color={"white"} />
+        </Box>
+        <StyledTextField multiline rows={5} />
+      </Box>
+      <Box sx={textFieldContainerStyles}>
+        <Box sx={textLabelStyles}>
+          <JetBrainsMonoText text={"Text Summary"} size={25} color={"white"} />
+        </Box>
+        <StyledTextField multiline rows={3} />
+      </Box>
+    </Box>
+  );
+};
+
+export default GptText;
