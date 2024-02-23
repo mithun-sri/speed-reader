00f8353d221161b15ec5a20ec94c1824228c6111
christin-s <christin.wulandari21@imperial.ac.uk>
2024-02-23 00:04:34 +0000

    [cyw321/#141] Submit text and questinos in GPT page to server

--- frontend/src/hooks/admin.ts
+++ frontend/src/hooks/admin.ts
@@ -1,5 +1,6 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { AdminApi } from "../api";
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
+import { AdminApi, QuestionWithCorrectOption, Text } from "../api";
 
 const adminApi = new AdminApi();
 
@@ -14,4 +15,32 @@ export function useGenerateText(difficulty: string, isFiction: boolean) {
     // Comment it out when we have data-fetching logic working.
     gcTime: 0,
   });
+}
+
+export function useAddText() {
+  return useMutation({
+    mutationFn: (text: Text) => adminApi.addText(text),
+  });
+}
+
+export function useSubmitQuestions() {
+  return useMutation({
+    mutationFn: submitQuestions,
+  });
+}
+
+interface SubmitQuestionsParams {
+  textId: string;
+  questions: QuestionWithCorrectOption[];
+}
+
+type SubmitQuestionsResponse = AxiosResponse;
+
+// Define the submitQuestions function
+async function submitQuestions({
+  textId,
+  questions,
+}: SubmitQuestionsParams): Promise<SubmitQuestionsResponse> {
+  // Call adminApi.submitQuestions and return the response
+  return adminApi.submitQuestions(textId, questions);
 }
