afe5dabe8aeaf8ea4f9d70f0cf818a7ae362273d
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-02-29 15:17:51 +0000

    [tm821/#175] Update frontend code using get_next_text() endpoint

--- frontend/src/views/AdaptiveMode/AdaptiveMode.tsx
+++ frontend/src/views/AdaptiveMode/AdaptiveMode.tsx
@@ -12,8 +12,8 @@ const AdaptiveModeView = () => {
   // When GPT get next text is set up use the following instead
   //const { wpm, setTextId, summarised } = useGameContext();
   //const { data: text } = summarised ? useNextSummarisedText() : useNextText();
-  const { wpm, setTextId } = useGameContext();
-  const { data: text } = useNextText();
+  const { wpm, setTextId, summarised } = useGameContext();
+  const { data: text } = useNextText(summarised);
 
   useEffect(() => {
     setTextId(text.id);
