fc859e5e489c497aec43ba74aa433d271ef8d298
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-03-02 13:57:23 +0000

    [tm821/#203] Add routes for mobile and desktop

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import NoMobile from "./components/Error/NoMobile";
 import NotFound from "./components/Error/NotFound";
 import ServerError from "./components/Error/ServerError";
 import LinearProgressFallback from "./components/LoadingBar/LinearProgressFallback";
@@ -10,14 +11,16 @@ import { SnackContextProvider } from "./context/SnackContext";
 import { WebGazerProvider } from "./context/WebGazerContext";
 import AdminRoute from "./routes/AdminRoute";
 import AuthRoute from "./routes/AuthRoute";
+import DesktopRoute from "./routes/DesktopRoute";
 import GuestRoute from "./routes/GuestRoute";
+import MobileRoute from "./routes/MobileRoute";
 import AdminAnalytics from "./views/Admin/Analytics";
 import GptView from "./views/Admin/GptView";
+import AvailableTexts from "./views/AvailableTexts/AvailableTexts";
 import { GamePage } from "./views/GameScreen/GameScreen";
 import Login from "./views/User/LogIn";
 import SignUp from "./views/User/SignUp";
 import UserView from "./views/User/UserView";
-import AvailableTexts from "./views/AvailableTexts/AvailableTexts";
 import WebGazerCalibration from "./views/WebGazerCalibration/WebGazerCalibration";
 
 function App() {
@@ -35,28 +38,33 @@ function App() {
               <WebGazerProvider>
                 <BrowserRouter>
                   <Routes>
-                    <Route element={<AuthRoute fallback="/login" />}>
-                      <Route path="/" element={<Navigate to="/game" />} />
-                      <Route path="/game" element={<GamePage />} />
-                      <Route
-                        path="/calibrate"
-                        element={<WebGazerCalibration />}
-                      />
-                      <Route path="/user" element={<UserView />} />
-                      <Route
-                        path="/available-texts"
-                        element={<AvailableTexts />}
-                      />
-                      <Route path="/gpt" element={<GptView />} />
-                      <Route element={<AdminRoute fallback="/login" />}>
-                        <Route path="/admin" element={<AdminAnalytics />} />
+                    <Route element={<DesktopRoute fallback="/mobile" />}>
+                      <Route element={<AuthRoute fallback="/login" />}>
+                        <Route path="/" element={<Navigate to="/game" />} />
+                        <Route path="/game" element={<GamePage />} />
+                        <Route
+                          path="/calibrate"
+                          element={<WebGazerCalibration />}
+                        />
+                        <Route path="/user" element={<UserView />} />
+                        <Route
+                          path="/available-texts"
+                          element={<AvailableTexts />}
+                        />
+                        <Route path="/gpt" element={<GptView />} />
+                        <Route element={<AdminRoute fallback="/login" />}>
+                          <Route path="/admin" element={<AdminAnalytics />} />
+                        </Route>
                       </Route>
+                      <Route element={<GuestRoute fallback="/game" />}>
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<SignUp />} />
+                      </Route>
+                      <Route path="*" element={<NotFound />} />
                     </Route>
-                    <Route element={<GuestRoute fallback="/game" />}>
-                      <Route path="/login" element={<Login />} />
-                      <Route path="/signup" element={<SignUp />} />
+                    <Route element={<MobileRoute fallback="/" />}>
+                      <Route path="/mobile" element={<NoMobile />} />
                     </Route>
-                    <Route path="*" element={<NotFound />} />
                   </Routes>
                 </BrowserRouter>
               </WebGazerProvider>
