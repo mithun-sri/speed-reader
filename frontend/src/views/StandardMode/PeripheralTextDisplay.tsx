ce30b4636210951703ba2e5138d8f73ebade3173
zk721 <ziya.khadaroo21@imperial.ac.uk>
2024-03-04 08:22:57 +0000

    [zk721/#139] Add pause and game progress bar logic, TODO: fix game from restarting at every keyboard interrupt and after this fix pause time logic for game progress bar

--- frontend/src/views/StandardMode/PeripheralTextDisplay.tsx
+++ frontend/src/views/StandardMode/PeripheralTextDisplay.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/system";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
+import { calculateAverageWpm } from "../../common/constants";
 import BlurBox from "../../components/Blur/Blur";
 import GameProgressBar from "../../components/ProgressBar/GameProgressBar";
 import { wpmAdjuster } from "../../components/WpmAdjuster/WpmAdjuster";
@@ -23,9 +24,9 @@ const PeripheralTextDisplay: React.FC<{
   const [curr_wpm, setWpm] = useState(wpm);
   const [isPaused, setPaused] = useState(false);
   const [scrollDuration, setScrollDuration] = useState(100);
+  const [progress, setProgress] = useState(0);
 
   // Assuming 10 words per line. This assumption can be made due to the fixed width of the container.
-  const lineHeight = 30; // in pixels (Used to calculate scroll speed)
   const containerWidth = 900; // in pixels
   const wordsPerLine = 10;
   const words = text.split(" ").length;
@@ -43,7 +44,6 @@ const PeripheralTextDisplay: React.FC<{
 
       // Multiply time per line by total number of lines to get the duration
       const duration = timePerLine * lines;
-      console.log("Duration: " + duration);
 
       return duration;
     }
@@ -68,15 +68,44 @@ const PeripheralTextDisplay: React.FC<{
     return () => window.removeEventListener("keydown", keyDownHandler);
   }, [curr_wpm, isPaused]);
 
-  const handleAnimationComplete = () => {
-    /*
-    const avg_wpm = calculateAverageWpm(intervalWpms);
-      setAverageWpm(avg_wpm);
+  useEffect(() => {
+    const startTime = Date.now();
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        const elapsedTime = Date.now() - startTime;
+        const progressPercentage =
+          (elapsedTime / (scrollDuration * 1000)) * 100;
 
-      incrementCurrentStage();
-      console.log("intervalWpms: ");
-      console.log(intervalWpms);
-      */
+        setProgress(progressPercentage);
+
+        if (progressPercentage >= 100) {
+          clearInterval(interval);
+          handleAnimationComplete();
+        }
+      }
+    }, 50); // Update every 50 milliseconds
+
+    return () => clearInterval(interval);
+  }, [scrollDuration, isPaused]);
+
+  // record WPM every 2.5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) {
+        setIntervalWpms([...intervalWpms, curr_wpm]);
+      }
+    }, 2500);
+
+    return () => clearInterval(interval);
+  }, [curr_wpm, intervalWpms, isPaused]);
+
+  const handleAnimationComplete = () => {
+    const avg_wpm = calculateAverageWpm(intervalWpms);
+    setAverageWpm(avg_wpm);
+
+    incrementCurrentStage();
+    console.log("intervalWpms: ");
+    console.log(intervalWpms);
   };
 
   return (
@@ -100,38 +129,74 @@ const PeripheralTextDisplay: React.FC<{
           flexWrap: "wrap",
           alignItems: "center",
           overflow: "hidden",
-          backgroundColor: "green",
+          backgroundColor: "#595d64",
+          borderRadius: "10px",
         }}
       >
-        <motion.p
-          initial={{ translateY: "100%" }}
-          animate={{ translateY: "-200%" }}
-          transition={{ duration: scrollDuration, ease: "linear" }}
-          style={{
-            fontSize: "22px",
-            fontFamily: "JetBrains Mono, monospace",
-            color: "#D1D0C5",
-            fontWeight: "bolder",
-            textAlign: "left",
-            lineHeight: `${lineHeight}px`,
-            position: "relative",
-            margin: 0, // Remove margin for accurate scrolling
-          }}
-          onAnimationComplete={handleAnimationComplete}
-        >
-          {text}
-        </motion.p>
-        <Box
-          sx={{
-            width: window.innerWidth / 2,
-            paddingTop: "200px",
-            paddingBottom: "10px",
-          }}
-        >
-          <GameProgressBar gameProgress={0} />
-        </Box>
+        <ScrollingText
+          text={text}
+          duration={scrollDuration}
+          isPaused={isPaused}
+        />
+      </Box>
+      <Box
+        sx={{
+          width: window.innerWidth / 2,
+          paddingTop: "200px",
+          paddingBottom: "10px",
+        }}
+      >
+        <GameProgressBar gameProgress={progress} />
       </Box>
     </Box>
+  );
+};
+
+interface ScrollingTextProps {
+  text: string;
+  duration: number;
+  isPaused?: boolean;
+}
+
+const ScrollingText: React.FC<ScrollingTextProps> = ({
+  text,
+  duration,
+  isPaused,
+}) => {
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isPaused) {
+      controls.stop(); // Pause the animation
+    } else {
+      controls.start({
+        translateY: ["100%", "-200%"],
+        transition: {
+          duration: duration,
+          ease: "linear",
+          from: "end",
+        },
+      });
+    }
+  }, [isPaused, duration, controls]);
+
+  return (
+    <motion.p
+      initial={{ translateY: "100%" }}
+      animate={controls}
+      style={{
+        fontSize: "22px",
+        fontFamily: "JetBrains Mono, monospace",
+        color: "#D1D0C5",
+        fontWeight: "bolder",
+        textAlign: "left",
+        lineHeight: "30px",
+        position: "relative",
+        margin: 0,
+      }}
+    >
+      {text}
+    </motion.p>
   );
 };
 
