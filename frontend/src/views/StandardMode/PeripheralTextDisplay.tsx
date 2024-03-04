2844f7035cbf5b5c072288035bc9f05e6f0ae51f
zk721 <ziya.khadaroo21@imperial.ac.uk>
2024-03-04 01:10:03 +0000

    [zk721/#139] Add blur and pause functionality and clean up logic for changing duration: TODO fix duration not updating during animation

--- frontend/src/views/StandardMode/PeripheralTextDisplay.tsx
+++ frontend/src/views/StandardMode/PeripheralTextDisplay.tsx
@@ -1,5 +1,11 @@
+import { Box } from "@mui/system";
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
+import BlurBox from "../../components/Blur/Blur";
+import GameProgressBar from "../../components/ProgressBar/GameProgressBar";
+import { wpmAdjuster } from "../../components/WpmAdjuster/WpmAdjuster";
+import WpmSign from "../../components/WpmSign/WpmSign";
+import { useGameContext } from "../../context/GameContext";
 import { useGameScreenContext } from "../GameScreen/GameScreen";
 
 /*
@@ -12,71 +18,120 @@ const PeripheralTextDisplay: React.FC<{
   size?: number;
 }> = ({ text, wpm }) => {
   const { incrementCurrentStage } = useGameScreenContext();
+  const { intervalWpms, setIntervalWpms, setAverageWpm } = useGameContext();
   const containerRef = useRef<HTMLDivElement>(null);
-  const [scrollDuration, setScrollDuration] = useState(0);
+  const [curr_wpm, setWpm] = useState(wpm);
+  const [isPaused, setPaused] = useState(false);
+  const [scrollDuration, setScrollDuration] = useState(100);
+
+  // Assuming 10 words per line. This assumption can be made due to the fixed width of the container.
   const lineHeight = 30; // in pixels (Used to calculate scroll speed)
   const containerWidth = 900; // in pixels
+  const wordsPerLine = 10;
+  const words = text.split(" ").length;
+  const lines = Math.ceil(words / wordsPerLine); // Estimate number of lines
+
+  // initialize intervalWpms list with initial wpm on component first render
+  useEffect(() => {
+    setIntervalWpms([wpm]);
+  }, []);
+
+  const calculateScrollDuration = () => {
+    if (words > 0 && containerRef.current) {
+      const linesPerSecond = curr_wpm / (60 * wordsPerLine);
+      const timePerLine = 1 / linesPerSecond;
+
+      // Multiply time per line by total number of lines to get the duration
+      const duration = timePerLine * lines;
+      console.log("Duration: " + duration);
+
+      return duration;
+    }
+    return scrollDuration;
+  };
 
   useEffect(() => {
-    const calculateScrollDuration = () => {
-      if (containerRef.current) {
-        // Assuming 10 words per line. This assumption can be made due to the fixed width of the container.
-        const wordsPerLine = 10;
-        const words = text.split(" ").length;
-        const lines = Math.ceil(words / wordsPerLine); // Estimate number of lines
-        const textHeight = lines * lineHeight; // Calculate container height taken up by the text
+    setScrollDuration(calculateScrollDuration());
+  }, [text, curr_wpm]);
 
-        const wordsPerSecond = wpm / 60;
-        const linesPerSecond = wordsPerSecond / 10;
-        const scrollSpeed = textHeight / linesPerSecond;
-        console.log("container height: " + textHeight);
-
-        const duration = (textHeight * 20) / scrollSpeed; // Calculate scroll duration
-        setScrollDuration(duration);
-        console.log("duration:" + duration);
+  // updates WPM based on keyboard event
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      if (event.key === " ") {
+        setPaused((prevPaused) => !prevPaused);
+      } else {
+        wpmAdjuster(event, curr_wpm, setWpm);
       }
     };
+    window.addEventListener("keydown", keyDownHandler);
 
-    calculateScrollDuration();
-  }, [text, wpm, scrollDuration]);
+    return () => window.removeEventListener("keydown", keyDownHandler);
+  }, [curr_wpm, isPaused]);
 
   const handleAnimationComplete = () => {
-    // Call incrementCurrentStage function when game ends
-    // incrementCurrentStage();
+    /*
+    const avg_wpm = calculateAverageWpm(intervalWpms);
+      setAverageWpm(avg_wpm);
+
+      incrementCurrentStage();
+      console.log("intervalWpms: ");
+      console.log(intervalWpms);
+      */
   };
 
   return (
-    <div
-      ref={containerRef}
-      style={{
-        width: `${containerWidth}px`, // Width is fixed to enforce ~10 words per line
-        height: `200px`, // Height is fixed
-        position: "relative",
+    <Box
+      sx={{
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
-        overflow: "hidden",
-        backgroundColor: "green",
       }}
     >
-      <motion.p
-        initial={{ translateY: "100%" }}
-        animate={{ translateY: "-200%" }}
-        transition={{ duration: 10, ease: "linear" }}
-        style={{
-          fontSize: "22px",
-          fontFamily: "JetBrains Mono, monospace",
-          color: "#D1D0C5",
-          fontWeight: "bolder",
-          textAlign: "left",
-          lineHeight: `${lineHeight}px`,
-          position: "relative",
-          margin: 0, // Remove margin for accurate scrolling
+      <WpmSign wpm={curr_wpm} />
+      {isPaused && <BlurBox />}
+      <Box
+        ref={containerRef}
+        sx={{
+          marginTop: "160px",
+          width: `${containerWidth}px`,
+          padding: "10px",
+          display: "flex",
+          height: "200px",
+          flexWrap: "wrap",
+          alignItems: "center",
+          overflow: "hidden",
+          backgroundColor: "green",
         }}
-        onAnimationComplete={handleAnimationComplete}
       >
-        {text}
-      </motion.p>
-    </div>
+        <motion.p
+          initial={{ translateY: "100%" }}
+          animate={{ translateY: "-200%" }}
+          transition={{ duration: scrollDuration, ease: "linear" }}
+          style={{
+            fontSize: "22px",
+            fontFamily: "JetBrains Mono, monospace",
+            color: "#D1D0C5",
+            fontWeight: "bolder",
+            textAlign: "left",
+            lineHeight: `${lineHeight}px`,
+            position: "relative",
+            margin: 0, // Remove margin for accurate scrolling
+          }}
+          onAnimationComplete={handleAnimationComplete}
+        >
+          {text}
+        </motion.p>
+        <Box
+          sx={{
+            width: window.innerWidth / 2,
+            paddingTop: "200px",
+            paddingBottom: "10px",
+          }}
+        >
+          <GameProgressBar gameProgress={0} />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
