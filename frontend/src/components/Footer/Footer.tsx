ae517f032c8938ed71761823f87397a2bb9aa8d0
Taeho <taehokimbusines@gmail.com>
2024-03-05 12:37:58 +0000

    [tk3718/#73] Add link to footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -67,7 +67,7 @@ const Footer = () => {
         </IconButton>
       </a>
       <a
-        href="https://www.discord.com"
+        href="https://discord.gg/5AraHjkw"
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -82,7 +82,7 @@ const Footer = () => {
           <FontAwesomeIcon icon={faDiscord} />
         </IconButton>
       </a>
-      <a href="https://www.x.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://twitter.com/speedreaderdoc" target="_blank" rel="noopener noreferrer">
         <IconButton
           style={{
             fontSize: iconSize,
