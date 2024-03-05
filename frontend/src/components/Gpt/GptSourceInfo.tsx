1389c994509a4b184d70777c1adf666f03c7c535
christin-s <christin.wulandari21@imperial.ac.uk>
2024-03-05 23:42:56 +0000

    [cyw321/#224] Create GPT textbox for source url, image url, description

--- frontend/src/components/Gpt/GptSourceInfo.tsx
+++ frontend/src/components/Gpt/GptSourceInfo.tsx
@@ -1,14 +1,10 @@
-import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Box, IconButton } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 import JetBrainsMonoText from "../Text/TextComponent";
 
 const GptSourceInfo: React.FC<{
   sourceTitle: string;
   author: string;
-  link: string;
-}> = ({ sourceTitle, author, link }) => {
+}> = ({ sourceTitle, author }) => {
   return (
     <Box
       sx={{
@@ -23,31 +19,16 @@ const GptSourceInfo: React.FC<{
           gap: 2,
         }}
       >
-        <JetBrainsMonoText text={"Source:"} size={25} color={"#E2B714"} />
+        <JetBrainsMonoText
+          text={"Title, Author:"}
+          size={25}
+          color={"#E2B714"}
+        />
         <JetBrainsMonoText
           text={sourceTitle + ", " + author}
           size={25}
           color={"#FFFFFF"}
         />
-        <IconButton
-          component={Link}
-          to={link}
-          target="_blank"
-          rel="noopener noreferrer" // To open in a new tab
-          sx={{
-            color: "#FFFFFF",
-            "& :hover": {
-              color: "#E2B714",
-            },
-            fontSize: "30px",
-            width: "50px",
-          }}
-        >
-          <FontAwesomeIcon
-            icon={faSquareArrowUpRight}
-            className="fa-table-page-icon"
-          />
-        </IconButton>
       </Box>
     </Box>
   );
